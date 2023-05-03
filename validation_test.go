6e99cb60570383b4bd0d2e501aeb7aea801f8565
Dzaky Mohammad <63341994+Dzaakk@users.noreply.github.com>
2023-05-03 19:32:16 +0700

    custom validation

--- validation_test.go
+++ validation_test.go
@@ -2,6 +2,7 @@ package govalidation
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/go-playground/validator/v10"
@@ -328,6 +329,38 @@ func TestAlias(t *testing.T) {
 	}
 
 	err := validate.Struct(seller)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func MustValidUsername(field validator.FieldLevel) bool {
+	value, ok := field.Field().Interface().(string)
+	if ok {
+		if value != strings.ToUpper(value) {
+			return false
+		}
+		if len(value) < 5 {
+			return false
+		}
+	}
+	return true
+}
+
+func TestCustomValidationFunction(t *testing.T) {
+	validate := validator.New()
+	validate.RegisterValidation("username", MustValidUsername)
+
+	type LoginRequest struct {
+		Username string `validate:"required,username"`
+		Password string `validate:"required"`
+	}
+
+	request := LoginRequest{
+		Username: "LIMAA",
+		Password: "",
+	}
+	err := validate.Struct(request)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
